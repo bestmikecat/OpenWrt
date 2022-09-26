4493f96792b989e93ff6e97e3def665f9246c9c0
bestmikecat <62850225+bestmikecat@users.noreply.github.com>
2022-09-26 19:20:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#开启MU-MIMO
+sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认wifi名称ssid
+sed -i 's/ssid=OpenWrt/ssid=BM/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认wifi密码
+sed -i 's/encryption=none/encryption=sae-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio${devidx}.encryption=sae-mixed/a\set wireless.default_radio${devidx}.key=Aa2247701304' package/kernel/mac80211/files/lib/wifi/mac80211.sh
